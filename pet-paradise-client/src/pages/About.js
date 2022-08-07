4f1bad9ab576a8b6aea64d55df0474e8a0c99fff
Kanwal Mehreen <88980986+kawiiii@users.noreply.github.com>
2022-08-07 14:34:58 +0500

    Update About.js

--- pet-paradise-client/src/pages/About.js
+++ pet-paradise-client/src/pages/About.js
@@ -8,6 +8,13 @@ const About = () => {
     <div>
     <Navbar/>
      <about>
+       <leftcontainer>
+          <h1>Who Are We ?</h1>
+          <p>“We work for the animal welfare.Our Mission is to end the suffering of abondoned animals around the world, by uniting not only the worlds leading animal welfare organisations but also the individuals in this nobel cause . ”</p>
+       </leftcontainer>
+       <rightcontainer>
+
+       </rightcontainer>
       
      </about>
     <Footer/>
